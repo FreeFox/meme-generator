4a6bd3d5983b56eab80556901183750516f5f9d3
Roman Lysiuk <roman.lysiuk@osf.digital>
2022-12-18 12:15:03 +0200

    state training

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,39 @@
 import './Main.css';
+import React from 'react';
+import memesData from '../memesData';
 
 function Main () {
+    //const [title, setTitle] = React.useState("Get a new meme image 🖼");
+    const title = "Get a new meme image 🖼";
+    const [memeImage, setMemeImage] = React.useState("");
+    
+    function getMemeImage (e) {
+        e.preventDefault();
+
+        var meme = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)];
+
+        //setTitle("Generating... 🖼");
+        setMemeImage(meme.url);
+
+        return false;
+    }
+
+    const titleMod = title.toUpperCase();
+
     return (
         <main className="main">
             <form action="#" className="generator-form">
                 <input type="text" name="top_text" placeholder="Top line" />
                 <input type="text" name="bottom_text" placeholder="Bottom line" />
-                <button type="submit" className="generate-button">Get a new meme image  🖼</button>
+                <button type="button"
+                    className="generate-button"
+                    onClick={getMemeImage}
+                    >
+                        {titleMod}
+                </button>
             </form>
+            {memeImage &&
+                <img src={memeImage} alt='Meme' className='meme-image' />}
         </main>
     );
 }
